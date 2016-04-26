f4c0c06dc12a0bc4da28535ad38a752462263f14
KunAn Hsu <annbigbig@gmail.com>
2016-04-26 23:15:42 +0800

    updated for Ubuntu 16.04 mate on Raspberry Pi 2/3 has released

--- README.md
+++ README.md
@@ -1,38 +1,7 @@
 # Lazy scripts
-These scripts are aimed to reduce the complexity of your daily adminstration works on Raspberry Pi 2.
+These scripts are aimed to reduce the complexity of your daily adminstration works on Ubuntu installed Arm/x64 platform
 
-it can perform these tasks for you , including :
-
-01_make_a_bootable_sd_card.sh
-```text
-Download Ubuntu 15.10 image for your Raspberry Pi 2 
-then flash the image into micro-SD card
-set the default display resolution to 1080p 60Hz
-finally it will resize your 2nd partition to the maximum size of your micro-SD
-after running this script, you will get a bootable micro-SD card for your Raspberry Pi 2.
-```
-02_miscellaneous.sh
-```text
-This script will do the following tasks for your Raspberry Pi 2
-1.Fix network interfaces name (To conventional 'eth0' and 'wlan0')
-2.Turn on tlp power save (Set TLP_ENABLE=1 in /etc/default/tlp) 
-3.Firewall rule setting (Write firewall rules in /etc/network/if-up.d/firewall)
-4.delete route to 169.254.0.0
-5.add swap space with 512MB
-6.install softwares you need
-```
-03_modify_sshd_settings.sh
-```text
-change the sshd service port number from 22(default) to whatever you preferred
-then add a public key that located at /tmp/public.key
-to /home/<USER_YOU_SPECIFIED>/.ssh/authorized_keys
-```
-04_install_x11vnc_and_make_it_autostart.sh
-```text
-install x11vnc and make it a permanate system service for you
-(listening on 127.0.0.1:5900)
-before run this script , you have to specify vnc password you preferred at top of the script for x11vnc service
-```
+their purpose was self-explanation by their file name
 
 I am not an expert so If you found any issue
 contact me : annbigbig@gmail.com
---
+++ raspberry_pi_2/ubuntu_mate_16.04/01_make_a_bootable_sd_card.sh
@@ -0,0 +1,116 @@
+#!/bin/sh
+# this script will help you make a bootable SD card for your Raspberry Pi 2
+# before excute it, please fill in the correct device name of your SD card here.
+SDCARD=/dev/sdc
+# WARNING!!! if you specify a wrong device, that maybe cause an unexpected result
+# (writing image file to wrong device or even destroy all of the data on your hard-disk)
+# HINT: you can run command 'sudo df -h' before and after plugging SD card into computer
+# to know what your device name of SD card extractly is.
+
+###
+calculate_last_sector() {
+	OFFSET=1
+	TOTAL_SECTORS=$(parted -s $SDCARD unit s print | grep $SDCARD | sed -r 's/^[^0-9]*([0-9]+).*/\1/')
+	return $(($TOTAL_SECTORS - $OFFSET))
+}
+
+flash_it() {
+	echo "umount SD card\n"
+	umount $SDCARD*
+	echo "done.\n"
+
+	#echo "checking SD card if any bad block exists on it ?\n"
+	#badblocks -n -v $SDCARD
+	#echo "done.\n"
+
+	echo "download the image file\n"
+	cd ~
+	mkdir rasp2_image
+	cd rasp2_image
+	wget http://can.ubuntu-mate.net/raspberry-pi/ubuntu-mate-16.04-desktop-armhf-raspberry-pi.img.xz
+	echo "download completed.\n"
+
+	echo "extracting image from xz file we just downloaded\n"
+	unxz -d ./ubuntu-mate-16.04-desktop-armhf-raspberry-pi.img.xz
+	echo "done.\n"
+
+	echo "flash image into SD card, start now ";date; echo "\n"
+	dd if=ubuntu-mate-16.04-desktop-armhf-raspberry-pi.img of=$SDCARD bs=1M
+	sync
+	echo "done. completed at ";date; echo "\n"
+
+}
+
+config_tunning() {
+
+	echo "config tunning"
+ 	mkdir /mnt/boot
+	mount ${SDCARD}1 /mnt/boot && cd /mnt/boot
+	sed -i -- "s|#hdmi_drive=2|hdmi_drive=2|g" ./config.txt
+	sed -i -- "s|#hdmi_group=1|hdmi_group=2|g" ./config.txt
+	sed -i -- "s|#hdmi_mode=1|hdmi_mode=82|g" ./config.txt
+	sed -i -- "s|#disable_overscan=1|disable_overscan=1|g" ./config.txt
+	sed -i -- "s|#hdmi_ignore_edid_audio=1|hdmi_ignore_edid_audio=1|g" ./config.txt
+	sync
+	cd /mnt
+	umount /mnt/boot && sleep 1
+	echo "done. display resolution changed to 1080p 60Hz"
+}
+
+resize_2nd_partition() {
+
+	echo "ready to resize 2nd partition, expend it to the maximum sector.\n"
+	echo "list the partition table first.\n"
+	parted -s $SDCARD unit s print
+	echo "done.\n"
+
+	echo "delete 2nd partition on SD card.\n"
+	parted -s $SDCARD rm 2
+	echo "done.\n"
+
+	echo "re-create 2nd partition, use the maximum size until the end of this SD card\n"
+	echo "your last sector number on SD card is $LAST_SECTOR \n"
+	parted -s $SDCARD unit s mkpart primary ext4 133120 $LAST_SECTOR
+	echo "done.\n"
+
+	echo "check 2nd partition.\n"
+	e2fsck -f ${SDCARD}2
+	echo "done.\n"
+
+	echo "resize it.\n"
+	resize2fs ${SDCARD}2
+	echo "done.\n"
+
+	echo "list the partition table again.\n"
+        parted -s $SDCARD unit s print
+        echo "done.\n"
+
+	sync
+}
+
+say_goodbye (){
+	echo "goodbye everyone"
+}
+
+main(){
+	calculate_last_sector
+	LAST_SECTOR=$?
+	echo -e "LAST_SECTOR of microSD card is $LAST_SECTOR \n"
+	flash_it
+	config_tunning
+	resize_2nd_partition
+	echo "now you can boot your Raspberry Pi 2 with this SD card."
+}
+
+echo "You are going to write Raspberry Pi 2 image file into device $SDCARD \n"
+read -p "Are you sure (y/n)?" sure
+case $sure in
+	[Yy]* )
+		main;
+		break;;
+	[Nn]* ) 
+		say_goodbye;
+		exit 1;;
+	* ) echo "Please answer yes or no."
+esac
+
