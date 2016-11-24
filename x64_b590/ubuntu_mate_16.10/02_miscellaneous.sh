9c8edae6c19202d8509d94c7c059c6d0bf4bd011
labasky <labasky@localhost.localdomain>
2016-11-24 20:05:15 +0800

    updated my laptop to ubuntu 16.10

---
+++ x64_b590/ubuntu_mate_16.10/02_miscellaneous.sh
@@ -0,0 +1,168 @@
+#!/bin/bash
+# This script will perform lots of work for fine tune Ubuntu 16.10 
+# before you run this script , please specify some parameters here:
+#
+LAN1="10.1.1.0/24" # The local network(1) that you allow packets come in from there
+LAN2="10.2.2.0/24" # The local network(2) that you allow packets come in from there
+VPN1="10.8.0.0/24" # The VPN network(1) that you allow packets come in from there
+VPN2="10.5.3.0/24" # The VPN network(2) that you allow packets come in from there
+#####################
+
+say_goodbye (){
+	echo "goodbye everyone"
+}
+
+fix_network_interfaces_name(){
+	ETH0_MAC_ADDRESS=$(ifconfig enp3s0 | grep ether | cut -d ' ' -f 10)
+	WLAN0_MAC_ADDRESS=$(ifconfig wlp2s0 | grep ether | cut -d ' ' -f 10)
+	NETWORK_RULES_FILE="/etc/udev/rules.d/70-network.rules"
+	touch $NETWORK_RULES_FILE
+	HOW_MANY_TIMES_ETH0_APPEAR=$(cat $NETWORK_RULES_FILE | grep -c "eth0")
+	HOW_MANY_TIMES_WLAN0_APPEAR=$(cat $NETWORK_RULES_FILE | grep -c "wlan0")
+	ZERO=0
+	HOW_MANY_LINES_IN_NETWORK_RULES_FILE=$(wc -l $NETWORK_RULES_FILE | cut -d ' ' -f 1)
+
+	if [ ! -z "$ETH0_MAC_ADDRESS" -a "$ETH0_MAC_ADDRESS" != " " -a $HOW_MANY_TIMES_ETH0_APPEAR -eq 0 ]; then
+		echo "mac address of eth0 : $ETH0_MAC_ADDRESS \n"
+		echo "SUBSYSTEM==\"net\", ACTION==\"add\", ATTR{address}==\"$ETH0_MAC_ADDRESS\", NAME=\"eth0\"" >> $NETWORK_RULES_FILE
+	fi
+
+	if [ ! -z "$WLAN0_MAC_ADDRESS" -a "$WLAN0_MAC_ADDRESS" != " " -a $HOW_MANY_TIMES_WLAN0_APPEAR -eq 0 ]; then
+		echo "mac address of wlan0 : $WLAN0_MAC_ADDRESS \n"
+		echo "SUBSYSTEM==\"net\", ACTION==\"add\", ATTR{address}==\"$WLAN0_MAC_ADDRESS\", NAME=\"wlan0\"" >> $NETWORK_RULES_FILE
+	fi
+
+	if [ $HOW_MANY_LINES_IN_NETWORK_RULES_FILE -eq 2 ]; then
+		echo "$NETWORK_RULES_FILE has been created successfully."
+	fi
+}
+
+firewall_setting(){
+        FIREWALL_RULE_FILE="/etc/network/if-up.d/firewall"
+        if [ ! -f $FIREWALL_RULE_FILE ]; then
+                echo "writing local firewall rule to $FIREWALL_RULE_FILE \n"
+                cat >> $FIREWALL_RULE_FILE << EOF
+#!/bin/sh
+# ============ Set your network parameters here ===================================================
+iptables=/sbin/iptables
+loopback=127.0.0.1
+local="\$(/sbin/ifconfig wlan0 | grep netmask | cut -d ' ' -f 10)"
+#local="\$(/sbin/ifconfig eth0 | grep netmask | cut -d ' ' -f 10)"
+#local=10.1.1.170
+lan1=$LAN1
+lan2=$LAN2
+vpn1=$VPN1
+vpn2=$VPN2
+# =================================================================================================
+\$iptables -t filter -F
+\$iptables -t filter -A INPUT -i lo -s \$loopback -d \$loopback -p all -j ACCEPT
+#\$iptables -t filter -A INPUT -i wlan0 -s \$local -d \$local -p all -j ACCEPT
+#\$iptables -t filter -A INPUT -i wlan0 -s \$lan1 -d \$local -p all -j ACCEPT
+#\$iptables -t filter -A INPUT -i wlan0 -s \$lan2 -d \$local -p all -j ACCEPT
+#\$iptables -t filter -A INPUT -i wlan0 -s \$vpn1 -d \$local -p all -j ACCEPT
+#\$iptables -t filter -A INPUT -i wlan0 -s \$vpn2 -d \$local -p all -j ACCEPT
+#\$iptables -t filter -A INPUT -i eth0 -s \$local -d \$local -p all -j ACCEPT
+#\$iptables -t filter -A INPUT -i eth0 -s \$lan1 -d \$local -p all -j ACCEPT
+#\$iptables -t filter -A INPUT -i eth0 -s \$lan2 -d \$local -p all -j ACCEPT
+#\$iptables -t filter -A INPUT -i eth0 -s \$vpn1 -d \$local -p all -j ACCEPT
+#\$iptables -t filter -A INPUT -i eth0 -s \$vpn2 -d \$local -p all -j ACCEPT
+\$iptables -t filter -A INPUT -s \$local -d \$local -p all -j ACCEPT
+\$iptables -t filter -A INPUT -s \$lan1 -d \$local -p all -j ACCEPT
+\$iptables -t filter -A INPUT -s \$lan2 -d \$local -p all -j ACCEPT
+\$iptables -t filter -A INPUT -s \$vpn1 -d \$local -p all -j ACCEPT
+\$iptables -t filter -A INPUT -s \$vpn2 -d \$local -p all -j ACCEPT
+\$iptables -t filter -A INPUT -p udp --dport 53 -j ACCEPT
+\$iptables -t filter -A INPUT -d \$local -p tcp --dport 36000 --syn -m state --state NEW -j ACCEPT
+\$iptables -t filter -A INPUT -s \$lan1 -p tcp --dport 36000 --syn -m state --state NEW -j ACCEPT
+\$iptables -t filter -A INPUT -s \$lan2 -p tcp --dport 36000 --syn -m state --state NEW -j ACCEPT
+\$iptables -t filter -A INPUT -s \$vpn1 -p tcp --dport 36000 --syn -m state --state NEW -j ACCEPT
+\$iptables -t filter -A INPUT -s \$vpn2 -p tcp --dport 36000 --syn -m state --state NEW -j ACCEPT
+\$iptables -t filter -A INPUT -d \$local -p tcp --dport 80 --syn -m state --state NEW -m limit --limit 160/s --limit-burst 200 -j ACCEPT
+\$iptables -t filter -A INPUT -d \$local -p tcp --dport 80 --syn -m state --state NEW -j DROP
+\$iptables -t filter -A INPUT -d \$local -p tcp --dport 443 --syn -m state --state NEW -m limit --limit 160/s --limit-burst 200 -j ACCEPT
+\$iptables -t filter -A INPUT -d \$local -p tcp --dport 443 --syn -m state --state NEW -j DROP
+\$iptables -t filter -A INPUT -s \$lan1 -d \$local -p icmp -j ACCEPT
+\$iptables -t filter -A INPUT -s \$lan2 -d \$local -p icmp -j ACCEPT
+\$iptables -t filter -A INPUT -s \$vpn1 -d \$local -p icmp -j ACCEPT
+\$iptables -t filter -A INPUT -s \$vpn2 -d \$local -p icmp -j ACCEPT
+\$iptables -t filter -A INPUT -p icmp --icmp-type 8 -m limit --limit 10/s -j ACCEPT
+\$iptables -t filter -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+\$iptables -t filter -P INPUT DROP
+\$iptables -t filter -L -n --line-number
+# =================================================================================================
+EOF
+		chmod +x $FIREWALL_RULE_FILE
+		echo "done.\n"
+        fi
+}
+
+delete_route_to_169_254_0_0(){
+	echo -e "delete route to 169.254.0.0/16 \n"
+        FILE1="/etc/network/if-up.d/avahi-autoipd"
+	sed -i -- "s|/bin/ip route|#/bin/ip route|g" $FILE1
+	sed -i -- "s|/sbin/route|#/sbin/route|g" $FILE1
+	echo -e "done.\n"
+}
+
+install_softwares(){
+        apt-get update
+	string="build-essential:git:htop:memtester:vim:subversion:synaptic:fcitx-table-boshiamy:gvncviewer"
+	IFS=':' read -r -a array <<< "$string"
+	for index in "${!array[@]}"
+	do
+           PACKAGE_COUNT=$((index+1))
+           PACKAGE_NAME=${array[index]}
+           #if [ -z "$(dpkg --get-selections | grep $PACKAGE_NAME)" ]; then
+              #echo "$PACKAGE_NAME was not installed on your system, install now ... "
+              apt-get install -y $PACKAGE_NAME
+           #else
+              #echo "$PACKAGE_NAME has been installed on your system."
+           #fi
+	done
+	apt-get -y upgrade
+	apt autoremove -y
+}
+
+install_chrome_browser() {
+	wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - 
+	sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+	apt-get update
+	apt-get install -y google-chrome-stable
+}
+
+fix_too_many_authentication_failures() {
+	sed -e '/pam_motd/ s/^#*/#/' -i /etc/pam.d/login
+	apt-get purge landscape-client landscape-common
+}
+
+main(){
+	fix_network_interfaces_name
+	firewall_setting
+	delete_route_to_169_254_0_0
+	install_softwares
+	install_chrome_browser
+	fix_too_many_authentication_failures
+	echo -e "now you should reboot your computer for configurations take affect.\n"
+	echo -e "RUN 'reboot' in your prompt # symbol\n"
+}
+
+echo -e "This script will do the following tasks for your Raspberry Pi 2, including: \n"
+echo -e "  1.Fix network interfaces name (To conventional 'eth0' and 'wlan0') \n"
+echo -e "  2.Firewall rule setting (Write firewall rules in /etc/network/if-up.d/firewall)\n"
+echo -e "  3.delete route to 169.254.0.0\n"
+echo -e "  4.install softwares you need\n"
+echo -e "  5.install chrome browser\n"
+echo -e "  6.fix too many authentication failures problem\n"
+
+read -p "Are you sure (y/n)?" sure
+case $sure in
+	[Yy]*)
+		main
+		;;
+	[Nn]*) 
+		say_goodbye
+		exit 1
+		;;
+	*) echo "Please answer yes or no."
+esac
+
---
+++ x64_b590/ubuntu_mate_16.10/06_install_nginx.sh
@@ -0,0 +1,186 @@
+#!/bin/bash
+
+#################
+
+say_goodbye() {
+        echo "goodbye everyone"
+}
+
+install_prerequisite() {
+	apt-get install -y build-essential
+	apt-get install -y libtool
+}
+
+remove_apache() {
+	echo "remove apache ..."
+	dpkg --get-selections | grep -v deinstall | grep apache2
+	apt-get remove --purge -y apache2
+	apt-get autoremove -y
+	echo "done."
+}
+
+install_nginx() {
+	echo "install nginx ..."
+	cd /usr/local/src
+	wget http://nginx.org/download/nginx-1.11.6.tar.gz
+	wget https://www.openssl.org/source/openssl-1.1.0c.tar.gz
+	wget ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.38.tar.gz
+	wget http://zlib.net/zlib-1.2.8.tar.gz
+
+	tar -zxvf ./nginx-1.11.6.tar.gz
+	tar -zxvf ./openssl-1.1.0c.tar.gz
+	tar -zxvf ./pcre-8.38.tar.gz
+	tar -zxvf ./zlib-1.2.8.tar.gz
+
+	chown -R root:root ./nginx-1.11.6
+	chown -R root:root ./openssl-1.1.0c
+	chown -R root:root ./pcre-8.38
+	chown -R root:root ./zlib-1.2.8
+
+	cd ./nginx-1.11.6
+	./configure --prefix=/usr/local/nginx-1.11.6 \
+	--user=www-data \
+	--group=www-data \
+	--with-http_ssl_module \
+	--with-pcre=/usr/local/src/pcre-8.38 \
+	--with-zlib=/usr/local/src/zlib-1.2.8 \
+	--with-openssl=/usr/local/src/openssl-1.1.0c \
+	--with-http_stub_status_module
+
+	make
+	make install
+
+	ln -s /usr/local/nginx-1.11.6 /usr/local/nginx
+
+	echo "done."
+}
+
+generate_config_file() {
+	echo "generating config file at /usr/local/nginx/conf/nginx.conf"
+	rm -rf /usr/local/nginx/conf/nginx.conf
+	cat >> /usr/local/nginx/conf/nginx.conf << "EOF"
+user www-data;
+worker_processes 2;
+pid logs/nginx.pid;
+
+events {
+        worker_connections 1024;
+        # multi_accept on;
+}
+
+http {
+
+    ##
+    # Basic Settings
+    ##
+
+    sendfile on;
+    tcp_nopush on;
+    tcp_nodelay on;
+    keepalive_timeout 65;
+    types_hash_max_size 2048;
+    # server_tokens off;
+
+    # server_names_hash_bucket_size 64;
+    # server_name_in_redirect off;
+
+    include mime.types;
+    default_type application/octet-stream;
+
+    ##
+    # Logging Settings
+    ##
+
+    access_log logs/access.log;
+    error_log logs/error.log;
+
+    ##
+    # Gzip Settings
+    ##
+
+    gzip on;
+    gzip_disable "msie6";
+
+    include /usr/local/nginx/conf.d/http.*.conf;
+}
+
+EOF
+
+	mkdir /usr/local/nginx/conf.d
+	cat >> /usr/local/nginx/conf.d/http.localhost.conf << "EOF"
+server {
+        listen 80 default_server;
+        #listen 127.0.0.1:80 default_server;
+        #listen [::]:80 default_server ipv6only=on;
+
+        access_log logs/localhost.access.log;
+        error_log logs/localhost.error.log;
+
+        root /usr/local/nginx/html;
+        index index.html index.htm;
+
+        # Make site accessible from http://localhost/
+        server_name localhost;
+
+        location / {
+                # First attempt to serve request as file, then
+                # as directory, then fall back to displaying a 404.
+                try_files $uri $uri/ /index.html;
+                # Uncomment to enable naxsi on this location
+                # include /etc/nginx/naxsi.rules
+        }
+}
+
+EOF
+	/usr/local/nginx/sbin/nginx -t -c /usr/local/nginx/conf/nginx.conf
+}
+
+post_installation() {
+	echo "generating /lib/systemd/system/nginx.service"
+cat >> /lib/systemd/system/nginx.service << "EOF"
+[Unit]
+Description=The NGINX HTTP and reverse proxy server
+After=syslog.target network.target remote-fs.target nss-lookup.target
+
+[Service]
+Type=forking
+PIDFile=/usr/local/nginx/logs/nginx.pid
+ExecStartPre=/usr/local/nginx/sbin/nginx -t
+ExecStart=/usr/local/nginx/sbin/nginx
+ExecReload=/bin/kill -s HUP $MAINPID
+ExecStop=/bin/kill -s QUIT $MAINPID
+PrivateTmp=true
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+	systemctl daemon-reload
+        systemctl enable nginx.service
+        systemctl start nginx.service
+        systemctl status nginx.service
+
+	echo "done."
+}
+
+main() {
+	echo "main() was called"
+	install_prerequisite
+	remove_apache
+	install_nginx
+	generate_config_file
+	post_installation
+}
+
+echo -e "This script will install nginx and make it as system service"
+read -p "Are you sure (y/n)?" sure
+case $sure in
+        [Yy]*)
+                main
+                ;;
+        [Nn]*)
+                say_goodbye
+                exit 1
+                ;;
+        *) echo "Please answer yes or no."
+esac
